46631b162a90654b73c574675209bae536b19556
vjp-davidMP <91985627+vjp-davidMP@users.noreply.github.com>
2023-05-25 00:33:38 +0200

    Punto 5 Terminado, Terminar informacion

--- src/environments/info_ayudas.ts
+++ src/environments/info_ayudas.ts
@@ -1,6 +1,10 @@
 export const info_ayudas = {
   listado_alarmas:{
     titulo:'Ayuda listado de alarmas',
-    contenido:'Esta pagina es para <br> ayudar '
+    contenido:'<p>La fecha es la fecha actual y cambia d&iacute;a tras d&iacute;a.</p>\n' +
+      '<p>CAMPO DE BUSQUEDA<br />El campo de b&uacute;squeda sirve para filtrar las alarmas a trav&eacute;s del texto que introducimos en este campo.</p>\n' +
+      '<p>B&Uacute;SQUEDA POR FECHA<br />En este campo se introduce la fecha que desee, y se te mostrara las alarmas que se hayan creado ese d&iacute;a, esto facilita la b&uacute;squeda de alarmas.</p>\n' +
+      '<p>ESTADO<br />En esta columna se muestra el estado de la alarma puede ser abierta, que significa que todav&iacute;a no esta resuelta la incidencia.<br />Pero tambi&eacute;n el estado puede ser resuelta, lo que significa que la incidencia ya esta solucionada.</p>\n' +
+      '<p>&nbsp;</p>'
   }
 }
