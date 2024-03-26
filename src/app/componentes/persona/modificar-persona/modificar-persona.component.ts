9f457139c642a6afe8d42c26a50c21a2f07d21f8
fsangar <fredericsangar@gmail.com>
2024-03-26 15:33:14 +0100

    Ampliado campos usuarios del servicio, añadido selector de provincias

--- src/app/componentes/persona/modificar-persona/modificar-persona.component.ts
+++ src/app/componentes/persona/modificar-persona/modificar-persona.component.ts
@@ -48,7 +48,11 @@ export class ModificarPersonaComponent implements OnInit {
   public plegado: boolean = false;
   public isAdmin: boolean;
   public id_tipo: number;
-
+  public listaProvincias: String[] = ['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres',
+  'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara',
+  'Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra',
+  'Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona',
+  'Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza']
 
   /* Constantes */
   readonly PLANTILLA_OBS = '';
